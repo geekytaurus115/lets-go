cf781c69cfc7fabd29b162aa9faabaf56a1b2e19
Mamata Shee <mps.ofc.3@gmail.com>
2024-05-14 20:04:59 +0530

    updated mysql-bookstore project

--- go-projects/004-mysql-bookstore-crud/go.mod
+++ go-projects/004-mysql-bookstore-crud/go.mod
@@ -1,3 +1,15 @@
 module github.com/geekytaurus115/go-mysql-bookstore-crud
 
 go 1.21.4
+
+require github.com/gorilla/mux v1.8.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/mysql v1.5.6 // indirect
+	gorm.io/gorm v1.25.10 // indirect
+)
--- go-projects/004-mysql-bookstore-crud/pkg/routes/bookstore-routes.go
+++ go-projects/004-mysql-bookstore-crud/pkg/routes/bookstore-routes.go
@@ -1,5 +1,16 @@
 package routes
 
-var RegisterBookStoreRoutes = func(route *mux.Route) {
+import (
+	"github.com/gorilla/mux"
+
+	"github.com/geekytaurus115/go-mysql-bookstore-crud/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/books/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 
 }
