9b6d4663a907c4daf5c68ecafa18bd5c50d0858f
Mamata Shee <mps.ofc.3@gmail.com>
2024-05-15 20:10:17 +0530

    saved changes

---
+++ go-concepts/array-slice/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var list [5]int
+
+	list[0] = 10
+	list[1] = 11
+	list[2] = 12
+	list[3] = 13
+	list[4] = 14
+
+	fmt.Println("List: ", list)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/geekytaurus115/lets-go
+
+go 1.21.4
